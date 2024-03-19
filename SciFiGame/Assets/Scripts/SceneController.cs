e95ff1eb5a11e69f52671c9cd389b39afbaec9e2
quinnb48 <qbehrens@reed.edu>
2024-03-19 10:37:54 +0100

    Button minigame now displays chosen order properly

--- SciFiGame/Assets/Scripts/SceneController.cs
+++ SciFiGame/Assets/Scripts/SceneController.cs
@@ -20,6 +20,7 @@ public class SceneController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //mainCamera = FindFirstObjectByType<Camera>();
         mainCamera = FindFirstObjectByType<Camera>().gameObject;
         //mainPlayer = FindFirstObjectByType<PlayerController>().gameObject;
         //pauseMenu = GetComponentInChildren<Canvas>();
