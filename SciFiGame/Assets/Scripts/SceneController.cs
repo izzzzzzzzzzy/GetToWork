c5a0b05dd03c8b1b483d5efb65c83b0c817ab877
quinnb48 <qbehrens@reed.edu>
2024-03-19 12:06:01 +0100

    button minigame progress; added correct/incorrect feedback, points, more buttons

--- SciFiGame/Assets/Scripts/SceneController.cs
+++ SciFiGame/Assets/Scripts/SceneController.cs
@@ -20,8 +20,8 @@ public class SceneController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //mainCamera = FindFirstObjectByType<Camera>();
-        mainCamera = FindFirstObjectByType<Camera>().gameObject;
+        mainCamera = FindFirstObjectByType<Camera>();
+        //mainCamera = FindFirstObjectByType<Camera>().gameObject;
         //mainPlayer = FindFirstObjectByType<PlayerController>().gameObject;
         //pauseMenu = GetComponentInChildren<Canvas>();
 
