2a54b6605782dc3a119427451aadcb1294337439
izzzzzzzzzzy <dkriz1662@gmail.com>
2024-05-02 12:03:32 +0200

    added 12:00 marker to clock

--- SciFiGame/Assets/Scripts/Clock.cs
+++ SciFiGame/Assets/Scripts/Clock.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class Clock : MonoBehaviour
 {
     [SerializeField] private float dayTime = 180f;
+    [SerializeField] private GameObject hand;
 
     public void SetAngle(float timeRemaining)
     {
         float angle = timeRemaining / dayTime * 360;
-        transform.rotation = Quaternion.Euler(0, 0, angle);
+        hand.transform.rotation = Quaternion.Euler(0, 0, angle);
     }
 }
