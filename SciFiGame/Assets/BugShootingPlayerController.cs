5f536dc295689f4d73dffed31903dc1bb68e54d6
Gemma-B <gemma@bertain.net>
2024-05-06 11:50:41 +0200

    Created Bug Shoot Minigame

--- SciFiGame/Assets/BugShootingPlayerController.cs
+++ SciFiGame/Assets/BugShootingPlayerController.cs
@@ -12,14 +12,17 @@ public class BugShootingPlayerController : PlayerBase
 
     public GameObject laser;
     public AudioClip laserSound;
+    public Animator animator;
 
     private float lastTimeFired = 0;
 
+    
     // Start is called before the first frame update
     void Start()
     {
         InitializeComponents();
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -37,11 +40,20 @@ public class BugShootingPlayerController : PlayerBase
     }
 
     void FireTheLasers(){
-        AudioSource.PlayClipAtPoint(laserSound, transform.position);
+        Quaternion rot = Quaternion.identity;
 
-		// Shooting up
-        inputDirect = new Vector3(lastInputDirection.x, lastInputDirection.y,0);
-		Instantiate(laser, transform.position + new Vector3(lastInputDirection.x, lastInputDirection.y,0), Quaternion.identity);
+        AnimatorClipInfo[] animInfo = this.animator.GetCurrentAnimatorClipInfo(0);
+
+        AudioSource.PlayClipAtPoint(laserSound, transform.position);
+        // Shooting up
+        inputDirect = new Vector3(lastInputDirection.x, lastInputDirection.y, 0);
+
+        if (animInfo[0].clip.name == "walking-front" || animInfo[0].clip.name == "walking-back" || animInfo[0].clip.name == "idle-front" || animInfo[0].clip.name == "idle-back") {
+
+            rot = Quaternion.Euler(0, 0, 90);
+        }
+
+            Instantiate(laser, transform.position + new Vector3(lastInputDirection.x, lastInputDirection.y, 0), rot);
 
     }
 }
